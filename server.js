f81d171b3cbb863633229df69941f4ff58278f2c
SManojPrabhakar <124668227+SManojPrabhakar@users.noreply.github.com>
2025-07-18 17:55:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,39 +3,39 @@ const path = require('path');
 const app = express();
 const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3000;
-const IP_ADDRESS="10.225.239.22"
+
 
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public')));
 
 
-const reportRoutes = require('C:/Users/vsdhe/Data/project/reportRoutes.js');
-const collageRoutes = require('C:/Users/vsdhe/Data/project/collegeRoutes.js');
-const register = require('C:/Users/vsdhe/Data/project/register.js');
-const routes = require('C:/Users/vsdhe/Data/project/pageroutes.js');
-const Login = require('C:/Users/vsdhe/Data/project/Loginroute.js');
-const signup = require('C:/Users/vsdhe/Data/project/signup.js');
-const Attend = require('C:/Users/vsdhe/Data/project/Attendance.js');
-const posts = require('C:/Users/vsdhe/Data/project/posts.js');
-const Userupdates = require('C:/Users/vsdhe/Data/project/Userupdates.js');
-const Fees = require('C:/Users/vsdhe/Data/project/Fees.js');
-const Passwords = require('C:/Users/vsdhe/Data/project/Passwords.js');
-const Blocks=require('C:/Users/vsdhe/Data/project/Blocks.js')
-const Delete=require('C:/Users/vsdhe/Data/project/Delete.js')
-const Notify=require('C:/Users/vsdhe/Data/project/PushNotifi.js');
-const Classes=require('C:/Users/vsdhe/Data/project/Classes.js')
-const Profile=require('C:/Users/vsdhe/Data/project/Profile.js')
-const sendOtpRoutes = require('C:/Users/vsdhe/Data/project/sendOtpRoutes.js');
-const webclassesRoutes = require('C:/Users/vsdhe/Data/project/webclasses.js');
-const webfeeRoutes= require('C:/Users/vsdhe/Data/project/webfees.js');
-const webattendance= require('C:/Users/vsdhe/Data/project/webattendance.js');
-const webupdate= require('C:/Users/vsdhe/Data/project/webupdate.js');
-const founder= require('C:/Users/vsdhe/Data/project/founder.js');
-const principal= require('C:/Users/vsdhe/Data/project/principal.js');
-const webprincipal= require('C:/Users/vsdhe/Data/project/webprinci.js');
-const exams= require('C:/Users/vsdhe/Data/project/exams.js');
-const hod= require('C:/Users/vsdhe/Data/project/hod.js');
+const reportRoutes = require('./reportRoutes.js');
+const collageRoutes = require('./collegeRoutes.js');
+const register = require('./register.js');
+const routes = require('./pageroutes.js');
+const Login = require('./Loginroute.js');
+const signup = require('./signup.js');
+const Attend = require('./Attendance.js');
+const posts = require('./posts.js');
+const Userupdates = require('./Userupdates.js');
+const Fees = require('./Fees.js');
+const Passwords = require('./Passwords.js');
+const Blocks=require('./Blocks.js')
+const Delete=require('./Delete.js')
+const Notify=require('./PushNotifi.js');
+const Classes=require('./Classes.js')
+const Profile=require('./Profile.js')
+const sendOtpRoutes = require('./sendOtpRoutes.js');
+const webclassesRoutes = require('./webclasses.js');
+const webfeeRoutes= require('./webfees.js');
+const webattendance= require('./webattendance.js');
+const webupdate= require('./webupdate.js');
+const founder= require('./founder.js');
+const principal= require('./principal.js');
+const webprincipal= require('./webprinci.js');
+const exams= require('./exams.js');
+const hod= require('./hod.js');
 // Middleware...
 
 app.use('/', routes);
@@ -65,7 +65,7 @@ app.use("/webprinci",webprincipal)
 app.use("/examcell",exams)
 app.use("/hod",hod)
 // Other routes...
-app.listen(PORT,IP_ADDRESS,() => {
+app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
   });
 
